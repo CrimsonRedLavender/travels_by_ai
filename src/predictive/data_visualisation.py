ca7d057318308d7cc44ba285496a74a2bd6c7404
lmerat-iut90 <leslie.merat@edu.univ-fcomte.fr>
2026-01-30 17:27:48 +0100

    refactor file

---
+++ src/predictive/data_visualisation.py
@@ -0,0 +1,49 @@
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('Clean_Dataset.csv', index_col=0)
+df.columns
+df['destination_city'].unique()[:10]
+
+
+# graphique des lignes aériaine par prix
+plt.figure(figsize=(12, 6))
+sns.barplot(data=df, x=df['airline'], y=df['price'], palette='magma')
+plt.title("Top Air lines by Price", fontsize=18, fontweight='bold')
+plt.xlabel("Air line")
+plt.ylabel("Price")
+sns.despine()
+plt.show()
+
+# graphique des parts des places éconnomiques et business
+plt.figure(figsize=(12, 6))
+df1 = df['class'].value_counts()
+df1
+plt.pie(
+    df1,
+    labels=['Economy', 'Business'],
+    autopct='%.0f%%',
+)
+plt.show()
+
+
+
+# graphique des prix par destination
+plt.figure(figsize=(12, 6))
+sns.barplot(data=df, x='destination_city', y='price', palette='magma')
+plt.show()
+
+
+## graphique durée par voyage
+top = df.groupby(['source_city', 'destination_city'])['duration'].mean().reset_index().sort_values(by='duration').head(
+    5)
+top
+plt.bar(
+    top['source_city'] + ' ' + top['destination_city'],
+    top['duration'],
+    color='skyblue',
+)
+plt.xticks(rotation=45)
+plt.show()
--- src/predictive/price_prediction.py
+++ src/predictive/price_prediction.py
@@ -1,7 +1,5 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline
@@ -16,58 +14,11 @@ from sklearn.tree import plot_tree
 
 
 
-
-
-
-
-
 df = pd.read_csv('Clean_Dataset.csv', index_col=0)
 df.columns
 df['destination_city'].unique()[:10]
 
 df = df.drop(['arrival_time', 'departure_time'], axis=1)
-
-
-
-# graphique des lignes aériaine par prix
-'''plt.figure(figsize=(12, 6))
-sns.barplot(data=df, x=df['airline'], y=df['price'], palette='magma')
-plt.title("Top Air lines by Price", fontsize=18, fontweight='bold')
-plt.xlabel("Air line")
-plt.ylabel("Price")
-sns.despine()
-plt.show()'''
-
-# graphique des parts des places éconnomiques et business
-'''plt.figure(figsize=(12, 6))
-df1 = df['class'].value_counts()
-df1
-plt.pie(
-    df1,
-    labels=['Economy', 'Business'],
-    autopct='%.0f%%',
-)
-plt.show()'''
-
-
-
-# graphique des prix par destination
-'''plt.figure(figsize=(12, 6))
-sns.barplot(data=df, x='destination_city', y='price', palette='magma')
-plt.show()'''
-
-
-## graphique durée par voyage
-'''top = df.groupby(['source_city', 'destination_city'])['duration'].mean().reset_index().sort_values(by='duration').head(
-    5)
-top
-plt.bar(
-    top['source_city'] + ' ' + top['destination_city'],
-    top['duration'],
-    color='skyblue',
-)
-plt.xticks(rotation=45)
-plt.show()'''
 
 
 X = df.drop(columns=['price'])
